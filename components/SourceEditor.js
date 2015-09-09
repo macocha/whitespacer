a1132f59e8ea6537c8e0499efc84ea2629fcd87e
Maciej Siwek <me@maciejsiwek.pl>
2015-09-09 15:20:39 +0200

    Added source code bleaching

--- components/SourceEditor.js
+++ components/SourceEditor.js
@@ -22,13 +22,15 @@ export default class SourceEditor extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      unbleach: false,
+    };
   }
 
   _handleChange(event) {
-    this.props.save(event.target.value);
+    this.props.save(this._bleach(event.target.value));
     this.props.parse();
   }
-
 
   _handleTabs(event) {
     if (event.keyCode === 9) {
@@ -39,17 +41,39 @@ export default class SourceEditor extends React.Component {
       event.target.value = val.substring(0, start) + '\t' + val.substring(end);
       event.target.selectionStart = event.target.selectionEnd = start + 1;
 
-      this.props.save(event.target.value);
+      this.props.save(this._bleach(event.target.value));
       this.props.parse();
       event.preventDefault();
     }
   }
 
+  _unbleach(code) {
+    return code.replace(/ /g, '[Space]').replace(/\n/g, '[LF]').replace(/\t/g, '[Tab]');
+  }
+
+  _bleach(code) {
+    return code.replace(/\[Space\]/g, ' ').replace(/\[LF\]/g, '\n').replace(/\[Tab\]/g, '\t').replace(/\[Space|\[LF|\[Tab/g, '');
+  }
+
+  _bleachChange(e) {
+    this.setState({
+      unbleach: e.target.checked,
+    });
+  }
+
+
   render() {
     return (
       <div style = {{...this.props.style, ...style}}>
-        <div style = {{flex: '0 0 40px'}}>Source code</div>
-        <textarea style={textareaStyle} value={this.props.code} onKeyDown = {(e) => this._handleTabs(e)} onChange = {(e) => this._handleChange(e)} />
+        <div style = {{flex: '0 0 40px'}}>
+          Source code
+          <label><input type="checkbox" checked = {this.state.unbleach} onChange={(e) => this._bleachChange(e)} /> Unbleach code</label>
+        </div>
+        <textarea style={textareaStyle}
+          value={this.state.unbleach ? this._unbleach(this.props.code) : this.props.code}
+          onKeyDown = {(e) => this._handleTabs(e)}
+          onChange = {(e) => this._handleChange(e)}
+        />
       </div>
     );
   }
